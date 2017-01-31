b11f0fe1e1287b42a4c73f0bf6c161667140965d
cis282student <mmendez9@dtcc.edu>
2017-01-31 12:09:45 -0500

    Broadcast

---
+++ app/src/main/java/edu/dtcc/cis282student/received/MyReceiver.java
@@ -0,0 +1,16 @@
+package edu.dtcc.cis282student.received;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Broadcast Received", Toast.LENGTH_LONG).show();
+    }
+}
